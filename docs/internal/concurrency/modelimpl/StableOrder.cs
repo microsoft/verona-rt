e1ab3ededbb7f91dcc42f920b5ff43ba11ca805c
Matthew Parkinson <mattpark@microsoft.com>
2023-03-03 12:57:04 +0000

    Simple version of stable order.

--- docs/internal/concurrency/modelimpl/StableOrder.cs
+++ docs/internal/concurrency/modelimpl/StableOrder.cs
@@ -1,48 +1,26 @@
 /**
  *  Used to provide a stable order over object.
  * 
- *  It initially uses HashCode to provide an order. If there 
- *  is a hashcode collision, then we use a counter to disambiguate between
- *  the two objects.
- * 
- *  Alternatively, we could just use an atomic on allocation. The design here
- *  aims to reduce the likelyhood of having to perform the atomic increment. 
+ *  Simple implementation that uses an atomic global counter at construction time.
  **/
 class StableOrder : IComparable
 {
-    // Used to disambiguate between two cowns that have the same hashcode.
+    // Source of unique identities. (Assuming not wrapping around a 64bit counter.)
     private static volatile int counter = 0;
 
-    // If we get a hashcode collision, then we need to use an additional value
-    // to distinguish between the two objects. We don't want to use this from
-    // the start as it requires Interlocked operations to create it.
-    private Int64 collision = 0;
+    // The identity of this object.
+    private Int64 identity = 0;
 
-    private void EnsureCollisionSet()
+    protected StableOrder()
     {
-        if (collision == 0)
-        {
-            var n = Interlocked.Increment(ref counter);
-            Interlocked.CompareExchange(ref collision, n, 0);
-        }
+        identity = Interlocked.Increment(ref counter);
     }
 
     public int CompareTo(object? obj)
     {
-        if (obj == null) return 1;
-
-        var result = this.GetHashCode().CompareTo(obj.GetHashCode());
-        if (result != 0) return result;
-
-        if (obj is StableOrder sobj)
+        if (obj is not null && obj is StableOrder sobj)
         {
-            // We are comparing two cowns with the same hashcode. We need to
-            // create a unique value for each cown to disambiguate between
-            // them.
-            EnsureCollisionSet();
-            sobj.EnsureCollisionSet();
-
-            return collision.CompareTo(sobj.collision);
+            return identity.CompareTo(sobj.identity);
         }
 
         return 1;
