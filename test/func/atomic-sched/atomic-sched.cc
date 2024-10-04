e974f3424b1802612e05f1a211f18127d49bfd42
Marios Kogias <marioskogias@gmail.com>
2024-10-04 16:25:34 +0100

    Add test to identify behaviour

--- test/func/atomic-sched/atomic-sched.cc
+++ test/func/atomic-sched/atomic-sched.cc
@@ -39,6 +39,99 @@ void test_body()
     });
 }
 
+void test_body_read_mixed()
+{
+  Logging::cout() << "test_body()" << Logging::endl;
+
+  auto log = make_cown<Body>();
+  auto log2 = make_cown<Body>();
+
+  (when(read(log)) <<
+   [=](acquired_cown<const Body> b) {
+     for (int i = 0; i < 10; i++)
+     {
+       Logging::cout() << "Behaviour 1\n";
+       // sleep(1);
+     }
+   }) +
+    (when(log2) << [=](auto) {
+      for (int i = 0; i < 10; i++)
+      {
+        Logging::cout() << "Behaviour 2\n";
+        // sleep(1);
+      }
+    });
+}
+
+void test_body_read_same1()
+{
+  Logging::cout() << "test_body()" << Logging::endl;
+
+  auto log = make_cown<Body>();
+
+  (when(read(log)) <<
+   [=](acquired_cown<const Body> b) {
+     for (int i = 0; i < 10; i++)
+     {
+       Logging::cout() << "Behaviour 1\n";
+       // sleep(1);
+     }
+   }) +
+    (when(log) << [=](auto) {
+      for (int i = 0; i < 10; i++)
+      {
+        Logging::cout() << "Behaviour 2\n";
+        // sleep(1);
+      }
+    });
+}
+
+void test_body_read_same2()
+{
+  Logging::cout() << "test_body()" << Logging::endl;
+
+  auto log = make_cown<Body>();
+
+  (when(log) <<
+   [=](auto b) {
+     for (int i = 0; i < 10; i++)
+     {
+       Logging::cout() << "Behaviour 1\n";
+       // sleep(1);
+     }
+   }) +
+    (when(read(log)) << [=](auto) {
+      for (int i = 0; i < 10; i++)
+      {
+        Logging::cout() << "Behaviour 2\n";
+        // sleep(1);
+      }
+    });
+}
+
+void test_body_read_only_same()
+{
+  Logging::cout() << "test_body()" << Logging::endl;
+
+  auto log = make_cown<Body>();
+
+  (when(read(log)) <<
+   [=](acquired_cown<const Body> b) {
+     for (int i = 0; i < 10; i++)
+     {
+       Logging::cout() << "Behaviour 1\n";
+       // sleep(1);
+     }
+   }) +
+    (when(read(log)) << [=](auto) {
+      for (int i = 0; i < 10; i++)
+      {
+        Logging::cout() << "Behaviour 2\n";
+        // sleep(1);
+      }
+    });
+}
+
 void test_body_same()
 {
   Logging::cout() << "test_body_same()" << Logging::endl;
@@ -90,9 +183,13 @@ int main(int argc, char** argv)
 {
   SystematicTestHarness harness(argc, argv);
 
-  harness.run(test_body);
-  harness.run(test_body_same);
-  harness.run(test_body_smart);
+  // harness.run(test_body);
+  // harness.run(test_body_read_mixed);
+  harness.run(test_body_read_only_same);
+  // harness.run(test_body_read_same1);
+  // harness.run(test_body_read_same2);
+  // harness.run(test_body_same);
+  // harness.run(test_body_smart);
 
   return 0;
 }
